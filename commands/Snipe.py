1c47ceda16abf7c03005d7d167f2d5ea7628a60a
Juhwooce <kryptstep@gmail.com>
2024-08-19 21:24:52 -0400

    Update Snipe.py

--- commands/Snipe.py
+++ commands/Snipe.py
@@ -2,6 +2,7 @@ from re import M
 import discord
 from discord.ext import commands
 import datetime
+from datetime import date
 import math
 import json
 from pytz import timezone
@@ -163,7 +164,7 @@ class Snipe(commands.Cog):
         session = Session()
         s = session.query(Servers.Servers).filter_by(server_id=server.id).first()
         sniped = s.recently_deleted_message
-        timestamp = datetime.datetime.strptime(s.recently_deleted_timestamp, f)
+        timestamp = s.recently_deleted_timestamp.strftime(f)
         images = s.recently_deleted_images.split("/")
         sniper = await self.bot.fetch_user(s.recently_deleted_user)
         u = session.query(User.User).filter_by(user_id=sniper.id).first()
@@ -214,7 +215,7 @@ class Snipe(commands.Cog):
         s = session.query(Servers.Servers).filter_by(server_id=server.id).first()
         messageB = s.recently_edited_before_message
         messageA = s.recently_edited_after_message
-        timestamp = datetime.datetime.strptime(s.recently_edited_timestamp, f)
+        timestamp = s.recently_edited_timestamp.strftime(f)
         images = s.recently_edited_images.split("/")
         snipee = await self.bot.fetch_user(s.recently_edited_user)
         reply = s.recently_edited_reply
