06c168b7d8835f329c173ce0cd1d15fec1b8b2b1
Juhwooce <kryptstep@gmail.com>
2024-08-27 21:08:16 -0400

    Update Stocks.py

--- commands/Stocks.py
+++ commands/Stocks.py
@@ -145,8 +145,7 @@ class Stocks(commands.Cog):
 
             embed = discord.Embed(title="Available Stocks", color=discord.Color.blue())
             for stock in stocks:
-                percent_change = 
-                direction = "up" if percent_change > 0 else "down"
+                percent_change = self.get_percentage_change()
                 embed.add_field(
                     name=stock.name,
                     value=f"Value: {stock.current_value:.2000f} discoins\nPercent Change: {percent_change:.2000f}%",
