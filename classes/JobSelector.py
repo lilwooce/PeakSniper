dade1e8bf832792b7755b077f431cf5499478fc3
Juhwooce <69056757+lilwooce@users.noreply.github.com>
2024-08-29 10:30:15 -0400

    Update JobSelector.py

--- classes/JobSelector.py
+++ classes/JobSelector.py
@@ -4,18 +4,18 @@ import json
 
 class JobSelection:
     def __init__(self, jobs_with_weights):
-        self.jobs = jobs_with_weights  # List of tuples (job_name, weight)
-        self.normalized_weights = [weight for _, weight in self.jobs.items()]
+        self.jobs = jobs_with_weights  # Dictionary {job_name: weight}
+        self.normalized_weights = [weight for weight in self.jobs.values()]
 
     def choose_job(self):
         if not self.jobs:
             return None  # No jobs to choose from
         
-        for _, weight in self.normalized_weights:
+        # Log each weight
+        for weight in self.normalized_weights:
             logging.warning(weight)
 
         # Select a job based on normalized weights
-        chosen_index = random.choices(range(len(self.jobs)), weights=self.normalized_weights)[0]
-        chosen_job = self.jobs[chosen_index]
+        chosen_job_name = random.choices(list(self.jobs.keys()), weights=self.normalized_weights)[0]
         
-        return chosen_job[0]  # Return the name of the chosen job
+        return chosen_job_name  # Return the name of the chosen job
